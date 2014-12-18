e32d16378fa7155fd605f3fd345a1af1258fa2a4
David Ceraso <davidceraso@athleticsnyc.com>
2014-12-18 10:41:31 -0500

    Beginning index and CSS assets.

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <!-- This is a simple example template that you can edit to create your own custom templates -->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Facebook sharing information tags -->
+        <meta property="og:title" content="*|MC:SUBJECT|*">
+        
+        <title>*|MC:SUBJECT|*</title>
+		
+		<link rel="stylesheet" href="assets/css/gimlet.css">
+
+	</head>
+    <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+    	<center>
+        	<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="backgroundTable">
+            	<tr>
+                	<td align="center" valign="top">
+                        <!-- // Begin Template Preheader \\ -->
+                        <table border="0" cellpadding="10" cellspacing="0" width="100%" id="templatePreheader">
+                            <tr>
+                                <td valign="top" class="preheaderContent">
+                                
+                                	<!-- // Begin Module: Standard Preheader \\ -->
+                                    <table border="0" cellpadding="10" cellspacing="0" width="660">
+                                    	<tr>
+                                            <td valign="top" width="100%">
+                                            	<div mc:edit="std_preheader_links">
+                                                	<!-- *|IFNOT:ARCHIVE_PAGE|* -->Doesn't look right? <a href="*|ARCHIVE|*" target="_blank">View it in your browser</a>.<!-- *|END:IF|* -->
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                	<!-- // End Module: Standard Preheader \\ -->
+                                
+                                </td>
+                            </tr>
+                        </table>
+                        <!-- // End Template Preheader \\ -->
+						<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" class="logoStripe">
+			            	<tr>
+			                	<td align="center" valign="top">
+			                        <table border="0" cellpadding="0" cellspacing="0" width="660" style="">
+			                            <tr>
+			                                <td valign="middle">
+			                                    <a href="http://hearstartup.com"><img src="https://gallery.mailchimp.com/0af2ad782b26fe31066f74a8b/images/80214ccf-7d7c-4cfa-9813-d8679d6e8e27.png" alt="StartUp"></a>  
+			                                </td>
+			                            </tr>
+			                        </table>
+			                    </td>
+			                </tr>
+			            </table>
+                    	<table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateContainer">
+                        	<tr>
+                            	<td align="center" valign="top">
+                                    <!-- // Begin Template Body \\ -->
+                                	<table border="0" cellpadding="0" cellspacing="0" width="660" id="templateBody">
+                                    	<tr>
+                                            <td valign="top" class="bodyContent">
+                                
+                                                <!-- // Begin Module: Standard Content \\ -->
+                                                <table border="0" cellpadding="25" cellspacing="0" width="100%">
+                                                    <tr>
+                                                        <td valign="top">
+                                                            <div mc:edit="std_content00">
+                                                                <span class="h1">Large title will go here</span>
+                                                                <p class="intro">Derit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id estT.</p>
+																<span class="h2">Subheading</span>
+                                                                After you enter your content, highlight the text you want to style and select the options you set in the style editor in the "styles" drop down box. Want to <a href="http://www.mailchimp.com/kb/article/im-using-the-style-designer-and-i-cant-get-my-formatting-to-change" target="_blank">get rid of styling on a bit of text</a>, but having trouble doing it? Just use the "clear styles" button to strip the text of any formatting and reset your style.
+																<br>
+                                                                <br>
+																<span class="h2">Subheading</span>
+                                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor <a href="#" target="_blank">incididunt</a> ut.
+																
+																<span class="h3">Subscribe Now</span>
+																<div class="share-buttons">
+				                                                    <a class="button" href="https://itunes.apple.com/us/podcast/startup-podcast/id913805339" target="_blank"><b>Subscribe in iTunes</b></a>
+																	<a class="button" href="http://feeds.hearstartup.com/hearstartup" target="_blank"><b>Subscribe to RSS</b></a>
+																</div>
+																
+																<a class="share-email" href="*|FORWARD|*">Share this email</a>
+                                                            </div>
+														</td>
+                                                    </tr>
+                                                </table>
+                                                <!-- // End Module: Standard Content \\ -->
+                                                
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <!-- // End Template Body \\ -->
+                                </td>
+                            </tr>
+                        	<tr>
+                            	<td align="center" valign="top">
+                                    <!-- // Begin Template Footer \\ -->
+                                	<table border="0" cellpadding="10" cellspacing="0" width="660" id="templateFooter">
+                                    	<tr>
+                                        	<td valign="top" class="footerContent">
+                                            
+                                                <!-- // Begin Module: Standard Footer \\ -->
+                                                <table border="0" cellpadding="25" cellspacing="0" width="100%">
+                                                    
+                                                    <tr>
+                                                        <td valign="top" align="center">
+															<div>
+			                                                	<a href="http://www.twitter.com/podcaststartup"><img src="https://gallery.mailchimp.com/0af2ad782b26fe31066f74a8b/images/3518dfc4-3ea4-4664-8eda-5ee96ef01f6c.png" width="30" height="30"></a>&nbsp;&nbsp;
+				                                                <a href="http://www.soundcloud.com/hearstartup"><img src="https://gallery.mailchimp.com/0af2ad782b26fe31066f74a8b/images/4cb27d25-187c-4c19-a2dc-e065da2ace82.png" width="30" height="30"></a>&nbsp;&nbsp;
+				                                                <a href="http://www.facebook.com/hearstartup"><img src="https://gallery.mailchimp.com/0af2ad782b26fe31066f74a8b/images/6880a0b8-07fe-48df-ae47-c6624386d2b4.png" width="30" height="30"></a>
+																<p>
+				                        						You are receiving this email because you provided us with your email address through the StartUp Sign Up page, by signing up for our newsletter, or by other means... If you no longer wish to receive emails from us, please <a href="*|UNSUB|*">unsubscribe</a> and you will be removed from our email list.
+																</p>
+																<p class="address">
+				                                                    *|HTML:LIST_ADDRESS_HTML|*
+				                                                </p>
+															</div>
+			                                            </td>
+                                                    </tr>
+                                                   
+                                                </table>
+                                                <!-- // End Module: Standard Footer \\ -->
+                                            
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <!-- // End Template Footer \\ -->
+                                </td>
+                            </tr>
+                        </table>
+                        <br>
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </body>
+</html>
